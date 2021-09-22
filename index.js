4878899e0378d8254ef03b5bdf908fcdc9e7778e
Yeison <enmanuelquiterios@gmail.com>
2021-09-22 10:22:48 -0400

    commit tree b6dcb86ada0f7ff13f56115600eec7aa848cdd12

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+var bestCustomer = "not bob";
+
+function upperCaseCustomerName(customerName) {
+  return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer;
+}
+setBestCustomer();
+function overwriteBestCustomer() {
+  return (bestCustomer = "maybe bob");
+}
+
+const leastFavoriteCustomer = "Least Favorite Customer";
+
+function changeLeastFavoriteCustomer() {
+  return (leastFavoriteCustomer = "Hello Checker");
+}
